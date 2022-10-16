03d2e4803b381ac73d8f56788295cd6f2cddc401
Azer444 <azerrh@code.edu.az>
2022-10-17 00:26:10 +0400

    Basket

---
+++ BasketTask/BasketTask/ViewModels/HomeVM.cs
@@ -0,0 +1,16 @@
+﻿using BasketTask.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BasketTask.ViewModels
+{
+    public class HomeVM
+    {
+        public IEnumerable<Slider> Sliders { get; set; }
+        public IEnumerable<Category> Categories { get; set; }
+        public IEnumerable<Product> Products { get; set; }
+        public SliderDetail SliderDetail { get; set; }
+    }
+}
